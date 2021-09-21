4c7ff58c1a7b26ceb76ed12229d1193086d99996
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-21 10:29:01 -0500

    chapter 3 Stack exercises

--- Ch03/03_02/Begin/Stack/Program.cs
+++ Ch03/03_02/Begin/Stack/Program.cs
@@ -10,7 +10,79 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            //TODO
+            Stack myStack = new Stack(4);
+            myStack.push("star wars");
+            myStack.push("the goonies");
+            myStack.push("The Godfather");
+            myStack.push("home alone 7: the loneliest home");
+            myStack.push("jaws");
+
+            Console.WriteLine("--------------------- lets take a peek: " +myStack.peek());
+
+            Console.WriteLine("--------------------- The stack contains ");
+            while (!myStack.isEmpty())
+            {
+                string movie = myStack.pop();
+                Console.WriteLine(movie);
+            }
+            string f = Console.ReadLine();
+        }
+    }
+
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        public Stack(int sizeInput)
+        {
+            maxSize = sizeInput;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+
+        public void push(string inputString)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("the stack is full");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = inputString;
+            }
+        }
+
+        private bool isFull()
+        {
+            return (maxSize - 1 == top);
+        }
+
+        public string pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("The stack is empty");
+                return "--";
+            }
+            else
+            {
+                int oldTop = top;
+                top--;
+                return stackArray[oldTop];
+            }
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        public string peek()
+        {
+            return stackArray[top];
         }
     }
 }
