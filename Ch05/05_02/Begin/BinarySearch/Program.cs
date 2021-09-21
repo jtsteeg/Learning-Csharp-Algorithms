4a975615429cab32950e781292c0f49a966a63f9
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-21 11:36:15 -0500

    chapter 5 binary search exercises

--- Ch05/05_02/Begin/BinarySearch/Program.cs
+++ Ch05/05_02/Begin/BinarySearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace BinarySearch
 {
@@ -6,6 +7,40 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int valueNeeded = 42;
+            int[] myArray = new int[] { 1, 4, 15, 18, 33, 40, 42, 50, 51, 65, 77, 82, 97, 100 };
+
+            WriteLine("our array contains: ");
+            Array.ForEach(myArray, x => Write(x + " "));
+
+            Write("\nthe result of our search for " + valueNeeded + " is: ");
+            WriteLine(binarySearch(myArray, valueNeeded));
+            ReadLine();
+        }
+
+        public static int binarySearch(int[] searchedArray, int numberToFind)
+        {
+            int firstIndex = 0;
+            int lastIndex = searchedArray.Length - 1;
+
+            while (firstIndex <= lastIndex)
+            {
+                int midpoint = (firstIndex + lastIndex) / 2;
+                if (numberToFind < searchedArray[midpoint])
+                {
+                    lastIndex = midpoint - 1;
+                }
+                else if(numberToFind > searchedArray[midpoint])
+                {
+                    firstIndex = midpoint + 1;
+                }
+                else
+                {
+                    return midpoint;
+                }
+            }
+
+            return -1;
         }
     }
 }
